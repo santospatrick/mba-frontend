4f45737711d3bd23c4ca2bcc968b5cab671bc138
Patrick Santos <patrickspteixeira@gmail.com>
2025-01-25 13:34:36 -0300

    feat: add axios, react-query and zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
 import Router from './Router'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './libs/react-query'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
